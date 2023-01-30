64640ad570dc4a36d9f17a078cd70dcf0f938e31
LeeSeungWoo <81506668+GGANCC1@users.noreply.github.com>
2023-01-30 21:53:44 +0900

    [Silver I] Title: 인간-컴퓨터 상호작용, Time: 496 ms, Memory: 174680 KB -BaekjoonHub

--- 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.py
+++ 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.py
@@ -4,16 +4,17 @@ string = str(sys.stdin.readline().rstrip())
 n = int(sys.stdin.readline().rstrip())
 prefix_sum = [[0] * 26 for _ in range(len(string))]
 for i in range(len(string)):
-    prefix_sum[i][ord(string[i]) - 97] += 1
+    prefix_sum[i][ord(string[i]) - 97] = 1
     for j in range(26):
         prefix_sum[i][j] += prefix_sum[i-1][j]
 for i in range(n):
-    s, a, b = map(str, sys.stdin.readline().rstrip().split())
+    s, a, b = list(sys.stdin.readline().split())
     a, b = int(a), int(b)
+    idx = ord(s) - 97
     if a == 0:
-        print(prefix_sum[b][ord(s) - 97])
+        print(prefix_sum[b][idx])
     else:
-        print(prefix_sum[b][ord(s) - 97] - prefix_sum[a - 1][ord(s) - 97])
+        print(prefix_sum[b][idx] - prefix_sum[a - 1][idx])
 
 
 
